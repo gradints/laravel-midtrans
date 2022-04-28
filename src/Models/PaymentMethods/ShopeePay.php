b7a26996179e37cfe9d0a9554b1f6f05b83bf527
Christhofer <christhofer@gradin.co.id>
2022-04-28 17:25:31 +0700

    wip

--- src/Models/PaymentMethods/ShopeePay.php
+++ src/Models/PaymentMethods/ShopeePay.php
@@ -17,46 +17,4 @@ class ShopeePay extends PaymentMethod
             'callback_url' => config('midtrans.redirect.finish'),
         ];
     }
-
-    // api
-    // [
-    //     'transaction_details' => [
-    //         'order_id' => 'inv_19042022_01',
-    //         'gross_amount' => 20_000,
-    //     ],
-    //     'custom_expiry' => [
-    //         'duration' => 1,
-    //         'init' => 'day' // second, minute, hour, day],
-    //     ]
-    //     'customer_details' => [
-    //         'firstName' => 'John',
-    //         'lastName' => 'Doe',
-    //         'email' => 'johnDoe@example.com'
-    //     ],
-    //     'payment_type' => 'shopeepay',
-    //     'shopeepay' => [
-    //       "callback_url": "https://midtrans.com/"
-    //     ],
-    // ];
-
-    // snap
-    // [
-    //     'transaction_details' => [
-    //         'order_id' => 'inv_19042022_01',
-    //         'gross_amount' => 20_000,
-    //     ],
-    //     'expiry' => [
-    //         'duration' => 1,
-    //         'init' => 'day' // second, minute, hour, day],
-    //     ]
-    //     'customer_details' => [
-    //         'firstName' => 'John',
-    //         'lastName' => 'Doe',
-    //         'email' => 'johnDoe@example.com'
-    //     ],
-    //     'enabled_payments' => ['shopeepay'],
-    //     'callbacks' => [
-    //         'finish' => 'https://demo.midtrans.com',
-    //     ],
-    // ];
 }
