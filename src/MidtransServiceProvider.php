deadebbda55b453fb4f18194840a34a0fec07d63
SLEEPLESS\rizky <rizkyputraednar@gmail.com>
2022-05-13 17:44:19 +0700

    move setup configuration midtrans

--- src/MidtransServiceProvider.php
+++ src/MidtransServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Gradints\LaravelMidtrans;
 
 use Illuminate\Support\ServiceProvider;
+use Midtrans\Config as MidtransConfig;
 
 class MidtransServiceProvider extends ServiceProvider
 {
@@ -31,5 +32,11 @@ class MidtransServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/NotificationAction.php' => app_path('Services/PaymentGateway/NotificationAction.php'),
         ], 'action');
+
+        // https://docs.midtrans.com/en/snap/integration-guide?id=sample-request
+        MidtransConfig::$serverKey = config('midtrans.server_key');
+        MidtransConfig::$isProduction = app()->environment('production');
+        MidtransConfig::$isSanitized = config('midtrans.use_sanitizer', false);
+        MidtransConfig::$is3ds = config('midtrans.enable_3ds', false);
     }
 }
